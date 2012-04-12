5489d15fc4718b786c3e85511cfe56a572beccc3
Igor Galić <i.galic@brainsware.org>
2012-04-12 19:58:54 +0200

    Introduce globals, try to initialize per-request.
    n.b.: This doesn't work. Yet.

--- src/dbd.c
+++ src/dbd.c
@@ -7,31 +7,57 @@
 #include <apr_dbd.h>
 #include <mod_dbd.h>
 
+ZEND_DECLARE_MODULE_GLOBALS(dbd)
+
 static zend_function_entry dbd_functions[] = {
-    ZEND_NS_FE(DBD_NS, prepare, NULL)
-    {NULL, NULL, NULL}
+  ZEND_NS_FE(DBD_NS, prepare, NULL)
+  {NULL, NULL, NULL}
 };
 
 zend_module_entry dbd_module_entry = {
 #if ZEND_MODULE_API_NO >= 20100525
-    STANDARD_MODULE_HEADER,
+  STANDARD_MODULE_HEADER,
 #endif
-    PHP_DBD_EXTNAME,
-    dbd_functions,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+  PHP_DBD_EXTNAME,
+  dbd_functions,
+  NULL,
+  NULL,
+  PHP_RINIT(dbd),
+  PHP_RSHUTDOWN(dbd),
+  NULL,
 #if ZEND_MODULE_API_NO >= 20100525
-    PHP_DBD_VERSION,
+  PHP_DBD_VERSION,
 #endif
-    STANDARD_MODULE_PROPERTIES
+  STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_DBD
 ZEND_GET_MODULE(dbd)
 #endif
+
+static void
+php_dbd_init_globals(zend_dbd_globals *dbd_globals)
+{
+  dbd_globals->r = NULL;
+  dbd_globals->d = NULL;
+  dbd_globals->pool = NULL;
+}
+
+PHP_RINIT_FUNCION(dbd)
+{
+  DBD_G(r) = SG(request_rec);
+  return SUCCESS;
+}
+
+PHP_RSHUTDOWN_FUNCTION(dbd)
+{
+  DBD_G(r) = NULL;
+  DBD_G(d) = NULL;
+  if (DBD_G(pool)) efree(DBD_G(pool));
+  DBD_G(pool) = NULL;
+
+  return SUCCESS;
+}
 
 PHP_FUNCTION(prepare)
 {
--- src/dbd.h
+++ src/dbd.h
@@ -1,6 +1,10 @@
 #ifndef PHP_DBD_H
 #define PHP_DBD_H 1
 
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
 #define PHP_DBD_VERSION "0.1"
 #define PHP_DBD_EXTNAME "DBD"
 #define DBD_NS "dbd"
@@ -9,13 +13,22 @@
 # include <apr_dbd.h>
 # include <mod_dbd.h>
 
-PHP_FUNCTION(prepare);
-
-struct dbd_t {
+ZEND_BEGIN_MODULE_GLOBALS(dbd)
   request_rec *r;
   ap_dbd_t *d;
-  const char *pool;
-};
+  char *pool;
+ZEND_END_MODULE_GLOBALS(dbd)
+
+#ifdef ZTS
+#define DBD_G(v) TSRMG(dbd_globals_id, zend_dbd_globals *, v)
+#else
+#define DBD_G(v) (dbd_globals.v)
+#endif
+
+PHP_RINIT_FUNCTION(dbd);
+PHP_RSHUTDOWN_FUNCTION(dbd);
+
+PHP_FUNCTION(prepare);
 
 extern zend_module_entry dbd_module_entry;
 #define phpext_dbd_ptr &dbd_module_entry
